95dbf5b0ba4a9b74e6da65f2e3c84b7397824d66
Michał Golub <michalgolab01@gmail.com>
2024-10-31 19:05:12 +0100

    31.10 - dodana encja Offers (nie zawiera propercji Amount)

--- StorageApp-75013b5447c736504e27e870b88cc0799bc1e411/StorageApp-75013b5447c736504e27e870b88cc0799bc1e411/StorageApp/Data/StorageAppDbContext.cs
+++ StorageApp-75013b5447c736504e27e870b88cc0799bc1e411/StorageApp-75013b5447c736504e27e870b88cc0799bc1e411/StorageApp/Data/StorageAppDbContext.cs
@@ -6,6 +6,8 @@ namespace StorageApp.Data
     public  class StorageAppDbContext : DbContext
     {
         public DbSet<Medicine> Medicines => Set<Medicine>();
+        public DbSet<Offer> Offers => Set<Offer>();
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
--- StorageApp-75013b5447c736504e27e870b88cc0799bc1e411/StorageApp-75013b5447c736504e27e870b88cc0799bc1e411/StorageApp/Program.cs
+++ StorageApp-75013b5447c736504e27e870b88cc0799bc1e411/StorageApp-75013b5447c736504e27e870b88cc0799bc1e411/StorageApp/Program.cs
@@ -9,11 +9,27 @@ medicineRepo.Add(new Medicine { Name = "Ibuprofen", Amount = 5 });
 medicineRepo.Add(new Medicine { Name = "Augmentin", Amount = 3 });
 medicineRepo.Save();
 
-var itemUpdated = new Medicine { Id = 1, Name = "Ibuprofen", Amount = 2 };
-medicineRepo.Update(itemUpdated);
+var itemMedicineUpdated = new Medicine { Id = 1, Name = "Ibuprofen", Amount = 2 };
+medicineRepo.Update(itemMedicineUpdated);
 medicineRepo.Save();
+
+var offerRepo = new SqlRepository<Offer>(new StorageAppDbContext());
+offerRepo.Add(new Offer { NameOfCompany = "Neuca" });
+offerRepo.Add(new Offer { NameOfCompany = "Medi" });
+offerRepo.Save();
+
+var itemOfferUpdated = new Offer { Id = 2, NameOfCompany = "Medi_System" };
+offerRepo.Update(itemOfferUpdated);
+offerRepo.Save();
+
 var repo = medicineRepo.GetAll();
 foreach (var item in repo)
+{
+    Console.WriteLine(item);
+}
+
+var repo1 = offerRepo.GetAll();
+foreach (var item in repo1)
 {
     Console.WriteLine(item);
 }
